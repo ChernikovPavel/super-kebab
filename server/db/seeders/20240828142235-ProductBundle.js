906ca6d20cd791b167517986ff9dfa99e9c9ff51
PavelChernickov <chernickov.pasha@gmail.com>
2024-08-29 22:29:52 +0300

    top seeds

--- server/db/seeders/20240828142235-ProductBundle.js
+++ server/db/seeders/20240828142235-ProductBundle.js
@@ -1,20 +1,24 @@
 'use strict';
+const {User, Product, Order} = require('../models')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+      const arr = []
+      const product = await Product.findAll({attributes: ['id']})
+      const orders = await Order.findAll({attributes: ['id']})
+      
+      for(let i = 0; i < 42; i++){
+        arr.push({
+          order_id: orders[Math.floor(Math.random() * orders.length)].dataValues.id,
+          product_id: product[Math.floor(Math.random() * product.length)].dataValues.id
+        })
+      }
+
+
     await queryInterface.bulkInsert(
       'ProductBundles',
-      [
-        {
-          product_id: 1,
-          order_id: 1,
-        },
-        {
-          product_id: 2,
-          order_id: 1,
-        },
-      ],
+      arr,
       {}
     );
   },
